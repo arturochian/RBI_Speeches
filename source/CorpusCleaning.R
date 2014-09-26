ad21b76eb98a90da85897cd6997eba6973cb46fe
christophergandrud <christopher.gandrud@gmail.com>
2014-09-26 11:58:32 +0200

    minor source cleaning

--- source/CorpusCleaning.R
+++ source/CorpusCleaning.R
@@ -1,18 +1,24 @@
-setwd('/git_repositories/RBI_Speeches/TextFiles')
+######################
+# Load and clean raw txt files
+#
+######################
 
-meta <- read.csv('/git_repositories/RBI_Speeches/metadata.csv', 
+setwd('/git_repositories/RBI_Speeches')
+
+meta <- read.csv('metadata.csv', 
                  stringsAsFactors = F)
 
 meta$textpath <- paste0(meta$textpath, '.txt')
 
-texts <- list()
 for (i in meta$textpath) {
     temp <-paste(readLines(i, encoding = 'Latin-1'), collapse = '')
     new_name <- gsub('/', '_', i)
     new_name <- gsub('TextFiles_', '', new_name)
-    writeLines(temp, paste0('AllRBISpeeches/', new_name))
+    writeLines(temp, paste0('TextFiles/AllRBISpeeches/', new_name))
 }
 
-texts <- VCorpus(DirSource(directory = 'AllRBISpeeches', encoding = "UTF-8"),
-                 readerControl = list(language = "en"))
+texts <- VCorpus(DirSource(directory = 'TextFiles/AllRBISpeeches', 
+                encoding = "UTF-8"),
+                readerControl = list(language = "en"))
+
 
